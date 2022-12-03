0c8ce9c9c9717f7642661e86ca82fca698416b8f
Md Alamin <alaminduc516@gmail.com>
2022-12-04 03:21:37 +0600

    Updated player selected button

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
 
-const selectedName = [];
-function playerAdded(){
+// const selectedName = [];
+// function playerAdded(){
+//     const playerListContainer = document.getElementById('selected-player');
+//     playerListContainer.innerText = '';
+//     for(let i  = 0; i < selectedName.length; i++){
+//         const li = document.createElement("li");
+//         li.innerHTML = selectedName[i];
+//         if(i < 5){
+//             playerListContainer.appendChild(li);
+//         }
+//         else{
+//             alert("Sorry! You can't add more then 5");
+//         }
+//     }
+// }
+
+// function selectPlayer(addPlayer){
+//     const playerName = addPlayer.parentNode.children[0].innerText
+//     console.log(playerName)
+//     if(selectedName.length < 5){
+//         selectedName.push(playerName);
+//         addPlayer.parentNode.disabled = true;
+//     }
+//     else{
+//         alert("Sorry! You can't add more then 5");
+//     }
+//     playerAdded();
+// }
+
+const selectedPlayerName = [];
+const addedPlayer = () =>{
     const playerListContainer = document.getElementById('selected-player');
-    playerListContainer.innerText = '';
-    for(let i  = 0; i < selectedName.length; i++){
+    playerListContainer.innerText = "";
+    for(let i = 0; i < selectedPlayerName.length; i++){
         const li = document.createElement("li");
-        li.innerHTML = selectedName[i];
+        li.innerHTML = selectedPlayerName[i];
         if(i < 5){
             playerListContainer.appendChild(li);
         }
@@ -15,22 +44,26 @@ function playerAdded(){
     }
 }
 
-function selectPlayer(addPlayer){
-    const playerName = addPlayer.parentNode.children[0].innerText
-    console.log(playerName)
-    if(selectedName.length < 5){
-        selectedName.push(playerName);
-        addPlayer.disabled = true;
+const selectPlayer = (add) => {
+    const playerName = add.parentNode.children[0].innerText;
+    if(selectedPlayerName.length < 5){
+        if(selectedPlayerName.indexOf(playerName) === -1){
+            selectedPlayerName.push(playerName);
+            add.innerText = 'selected';
+            add.style.background = 'gray';
+        }else{
+            alert('Already added');
+        }
+    }else{
+        alert('You can\'t add more than 5 player');
     }
-    else{
-        alert("Sorry! You can't add more then 5");
-    }
-    playerAdded();
+    addedPlayer();
 }
+
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const perPlayerDemand = getInputValue('per-player-field');
-    const totalPlayer = selectedName.length;
+    const totalPlayer = selectedPlayerName.length;
     const playerExpense = document.getElementById('player-expenses');
     if(totalPlayer.length < 6 || (isNaN(perPlayerDemand) === false )){
        const totalPlayerExpense = perPlayerDemand * totalPlayer;
